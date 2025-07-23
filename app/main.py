f47762133e44e6fed8b7507b93a2219de7bd906a
Yurii Palahitskyi <brebston7@gmail.com>
2025-07-23 11:47:38 +0200

    Fixed problems

--- app/main.py
+++ app/main.py
@@ -1,19 +1,28 @@
+from __future__ import annotations
+
+
 class Distance:
     def __init__(self, km: float) -> None:
         self.km = km
 
     def __str__(self) -> str:
-        return f"Distance: {self.km} kilometers."
+        return "Distance: {self.km} kilometers."
 
     def __repr__(self) -> str:
-        return f"Distance(km={self.km})"
+        return "Distance(km={self.km})"
 
-    def __add__(self, other: "Distance") -> "Distance":
+    def __add__(self, other: "Distance" | int | float) -> "Distance":
         if isinstance(other, Distance):
             return Distance(self.km + other.km)
         if isinstance(other, (int, float)):
             return Distance(self.km + other)
         return NotImplemented
+
+    def __radd__(self, other: int | float) -> "Distance":
+        return self.__add__(other)
+
+    def __rmul__(self, other: int | float) -> "Distance":
+        return self.__mul__(other)
 
     def __iadd__(self, other: "Distance") -> "Distance":
         if isinstance(other, Distance):
@@ -24,7 +33,7 @@ class Distance:
             return NotImplemented
         return self
 
-    def __mul__(self, other: "Distance") -> "Distance":
+    def __mul__(self, other: "Distance" | int | float) -> "Distance":
         if isinstance(other, (int, float)):
             return Distance(self.km * other)
         return NotImplemented
